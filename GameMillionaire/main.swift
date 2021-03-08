be125b014b504d98bf091db58b9709262aabf28f
Letícia Lima <leticialima@leticias-macbook-air.local>
2021-03-08 07:39:08 -0500

    Initial Commit

---
+++ GameMillionaire/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GameMillionaire
+//
+//  Created by Letícia Lima on 2021-03-08.
+//  Copyright © 2021 Wanja. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
